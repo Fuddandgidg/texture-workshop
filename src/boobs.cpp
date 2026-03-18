6011e728ed85a817962fdaa567a761d0b00e976d
Fuddandgidg <buttercupprn@gmail.com>
2026-03-18 11:37:32 -0400

    Update boobs.cpp

--- src/boobs.cpp
+++ src/boobs.cpp
@@ -1,36 +1,45 @@
 #include <Geode/Geode.hpp>
-
-using namespace geode::prelude;
-
 #include <Geode/modify/MenuLayer.hpp>
 #include "TextureWorkshopLayer.hpp"
 #include "boobs.hpp"
-#include <Geode/utils/web.hpp>
-#include <Geode/loader/Mod.hpp>
 
+using namespace geode::prelude;
+
+// Hook into the MenuLayer to add the Workshop button
 class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
+    bool init() {
+        if (!MenuLayer::init()) {
+            return false;
+        }
 
-		auto wsButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("TWS_MainButton.png"_spr),
-			this,
-			menu_selector(MyMenuLayer::onMyButton)
-		);
-		auto menu = this->getChildByID("right-side-menu");
-		menu->addChild(wsButton);
-		wsButton->setID("workshop-button"_spr);
-		menu->updateLayout();
-		if (Mod::get()->getSavedValue<bool>("version-filter")) {
-			boobs::versionFilter = Mod::get()->getSavedValue<bool>("version-filter");
-		}
-		return true;
-	}
+        // 2.2081: Use the standard right-side-menu ID
+        auto menu = this->getChildByID("right-side-menu");
+        if (menu) {
+            auto wsButton = CCMenuItemSpriteExtra::create(
+                CCSprite::createWithSpriteFrameName("TWS_MainButton.png"_spr),
+                this,
+                menu_selector(MyMenuLayer::onMyButton)
+            );
+            
+            wsButton->setID("workshop-button"); // Cleaned up ID for 2.2081
+            menu->addChild(wsButton);
+            
+            // Critical for 2.2081: Refresh the menu layout so the button shows up
+            menu->updateLayout();
+        }
 
-	void onMyButton(CCObject*) {
-		auto workshopLayer = TextureWorkshopLayer::scene();
-		CCDirector::sharedDirector()->pushScene(CCTransitionFade::create(0.5f, workshopLayer));
-	}
+        // Load your saved filter settings
+        if (Mod::get()->hasSavedValue("version-filter")) {
+            boobs::versionFilter = Mod::get()->getSavedValue<bool>("version-filter");
+        }
+
+        return true;
+    }
+
+    void onMyButton(CCObject*) {
+        auto workshopLayer = TextureWorkshopLayer::scene();
+        // 2.2081: Standard fade transition
+        auto transition = CCTransitionFade::create(0.5f, workshopLayer);
+        CCDirector::get()->pushScene(transition);
+    }
 };
