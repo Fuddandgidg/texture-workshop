d33bfc466a7e0d8cd280789844f3f571b4208f2f
Fuddandgidg <buttercupprn@gmail.com>
2026-03-18 11:38:18 -0400

    Update boobs.hpp

--- src/boobs.hpp
+++ src/boobs.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <Geode/Geode.hpp>
+#include <matjson.hpp>
+#include "TexturePack.hpp"
+
 using namespace geode::prelude;
 
 class boobs {
     public:
+        // 2.2081: Use inline static for global access across all mod files
         inline static bool downloaded = false;
         inline static bool versionFilter = true;
-        inline static matjson::Value tpJson; // nothing to see here!
+        
+        // This holds your test.json data once it's loaded
+        inline static matjson::Value tpJson; 
+        
+        // Keeps track of active downloads to prevent duplicates
         inline static std::vector<TexturePack*> downloads = { };
 };
